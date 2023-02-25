9a84defba04c15a6cb54b906ec72f7c0ce32672d
Yago Macinelli <yagom.ym@gmail.com>
2023-02-24 21:55:26 -0300

    Alterado: => Códigos da main inseridos dentro de funções.

--- fundamentos_dart/bin/fundamentos_dart.dart
+++ fundamentos_dart/bin/fundamentos_dart.dart
@@ -3,18 +3,26 @@ import 'dart:io';
 import 'package:fundamentos_dart/classes/Produto.dart';
 import 'package:fundamentos_dart/fundamentos_dart.dart' as fundamentos_dart;
 
+String? nome = 'Douglas';
+String? sobrenome = 'Maycon';
 double mediaAprovacao = 7.0;
 
 void main(List<String> arguments) {
-  int? idade;
-  String? nome;
-  String? sobrenome;
-  double? salario;
-  bool? isDeveloper;
 
+  printSQL();
+  atividade01();
+  atividade02();
+  print("Aluno aprovado: " + verificarAprovacao(5, 8.5, 3.6).toString());
+}
+
+void printSQL() {
   print("INSERT INTO pessoa (nome, sobrenome) VALUES ('$nome', '$sobrenome')");
+}
 
-  //Atividade 01 -> Defina variáveis de um produto (com diversos tipos) e imprima com as devidas descrições.
+/**
+ * Atividade 01 -> Defina variáveis de um produto (com diversos tipos) e imprima com as devidas descrições.
+ */
+void atividade01() {
   Produto produto = new Produto();
   produto.produto = 'Detergente 500ml';
   produto.marca = 'Ypê';
@@ -23,11 +31,13 @@ void main(List<String> arguments) {
   print("Descrição produto: " + produto.produto.toString());
   print("Marca: " + produto.marca.toString());
   print("Valor: R\$ " + produto.valor.toString());
+}
 
-  //Atividade 02 -> Conforme o seu projeto particular defina as variáveis de um modelo e imprima na tela.
-
-  //Atividade 03 -> Inserção de dados.
-
+/**
+ * Atividade 02 -> Conforme o seu projeto particular defina as variáveis de um modelo e imprima na tela.
+ * Atividade 03 -> Inserção de dados.
+ */
+void atividade02() {
   print("Insira seu nome e sobrenome: ");
   nome = stdin.readLineSync();
   sobrenome = stdin.readLineSync();
@@ -37,12 +47,11 @@ void main(List<String> arguments) {
   $nome
   $sobrenome
   ''');
-
-  print("Aluno aprovado: " + verificarAprovacao(5, 8.5, 3.6).toString());
 }
 
 /**
- * Função para verificação de média trimestral
+ * Função para verificação de média trimestral.
+ * @param nota1, nota2, nota3
  */
 bool verificarAprovacao(double nota1, double nota2, double nota3) {
   double media = (nota1 + nota2 + nota3) / 3;
