21f65a1843bd395d45245562f31d28d198dc1c9f
Yago Macinelli <yagom.ym@gmail.com>
2023-02-27 17:47:50 -0300

    Add: => Add Digit function.

--- calculadora/lib/memory.dart
+++ calculadora/lib/memory.dart
@@ -1,6 +1,6 @@
 
 class Memory {
-  static const operations = ['%', '+', '-', '*', '='];
+  static const operations = ['%', '+', '-', 'x', '='];
   String? operator;
   bool usedOperation = false;
   int bufferIndex = 0;
@@ -11,6 +11,18 @@ class Memory {
     clear();
   }
 
+  void applyCommand(String command) {
+    if (command == 'AC') {
+      clear();
+    } else if (command == 'DEL') {
+      deleteLastDigit();
+    } else if (operations.contains(command)) {
+      setOperation(command);
+    } else {
+      addDigit(command);
+    }
+  }
+
   void clear() {
     result = '0';
     operator = null;
@@ -18,10 +30,21 @@ class Memory {
     bufferIndex = 0;
     buffer.setAll(0, [0.0, 0.0]);
   }
+  
+  void deleteLastDigit() {
+    result = result.length > 1 ? result.substring(0, result.length - 1) : '0';
+  }
 
-  void applyCommand(String command) {
-    if (command == 'AC') {
-      clear();
-    }
+  void setOperation(String command) {
+
+  }
+
+  void addDigit(String digit) {
+    if (usedOperation) result = '0';
+    if (result.contains('.') && digit == '.') digit = '';
+    if (result == '0' && digit != '.') result = '';
+
+    result += digit;
+    buffer[bufferIndex] = double.parse(result);
   }
 }
